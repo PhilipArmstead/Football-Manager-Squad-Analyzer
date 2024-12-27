76e5249c455891b3d9819ae4e1939373bf1acac6
Phil Armstead <philarmstead@mailbox.org>
2024-12-27 11:23:05 +0000

    Printing player details

--- main.c
+++ main.c
@@ -2,11 +2,11 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
 
-static inline long hexBytesToInt(const unsigned char *bytes,
-                                 const char length) {
+void printPlayer(const int fd, const long address);
+
+static inline long hexBytesToInt(const unsigned char *bytes, const char length) {
   long value = 0;
   for (short i = 0; i < length; ++i) {
     value += (long)(bytes[i] * pow(256, i));
@@ -14,7 +14,7 @@ static inline long hexBytesToInt(const unsigned char *bytes,
   return value;
 }
 
-static inline void resetAttributes(const int fd, const int address) {
+static inline void resetAttributes(const int fd, const long address) {
   const char max = 100;
   const char min = 1;
 
@@ -36,7 +36,18 @@ static inline void resetAttributes(const int fd, const int address) {
   const char twenty = 20;
   for (short i = 0; i < 8; ++i) {
     write (fd, &twenty, 1);
-  }}
+  }
+}
+
+static inline void readFromMemory(const int fd, const long address, const int length, unsigned char *bytes) {
+  lseek(fd, address, SEEK_SET);
+  read(fd, bytes, length);
+}
+
+static inline void writeToMemory(const int fd, const long address, const int length, unsigned char *bytes) {
+  lseek(fd, address, SEEK_SET);
+  write(fd, bytes, length);
+}
 
 int main() {
   /**
@@ -70,38 +81,84 @@ int main() {
 
   unsigned char bytes[4];
   const long pointerAttributes = 0x15DCD9415;
-  lseek(fileDescriptorMemory, pointerAttributes, SEEK_SET);
-  read(fileDescriptorMemory, &bytes, 4);
+  readFromMemory(fileDescriptorMemory, pointerAttributes, 4, bytes);
   const long memoryLocationAttributes = hexBytesToInt(bytes, 4);
-  lseek(fileDescriptorMemory, memoryLocationAttributes - 0x76, SEEK_SET);
-  read(fileDescriptorMemory, &bytes, 1);
 
-  printf("  %d\n", bytes[0]);
+  // resetAttributes(fileDescriptorMemory, memoryLocationAttributes);
 
-  resetAttributes(fileDescriptorMemory, memoryLocationAttributes);
+  // while (1) {
+    // printf("\n");
+    // signed short input;
 
-  while (1) {
-    printf("\n");
-    signed short input;
+    // printf("Offset from attribute base: ");
+    // scanf("%d", &input);
+    // signed short offset = input;
+    // printf("New value (0-255): ");
+    // scanf("%d", &input);
+    // unsigned char value = input;
 
-    printf("Offset from attribute base: ");
-    scanf("%d", &input);
-    signed short offset = input;
-    printf("New value (0-255): ");
-    scanf("%d", &input);
-    unsigned char value = input;
-
-    lseek(fileDescriptorMemory, memoryLocationAttributes + offset, SEEK_SET);
-    write (fileDescriptorMemory, &value, 1);
-  }
-
-  // byte = 200;
-  // lseek(fileDescriptorMemory, addr, SEEK_SET);
-  // write (fileDescriptorMemory, &byte, len     );
-  // if (write (fileDescriptorMemory, byte , len     ) == -1) {
-  //    printf("Error while writing\n");
-  //    exit(1);
+    // writeToMemory(fileDescriptorMemory, memoryLocationAttributes + offset, 1, &value);
   // }
 
+  printPlayer(fileDescriptorMemory, memoryLocationAttributes);
+
   free(proc_mem);
+}
+
+static inline void padLeft(const unsigned char byte) {if (byte < 10) {
+  printf(" ");
+}
+  printf("%d", byte);
+}
+
+void printPlayer(const int fd, const long address) {
+  unsigned char bytes[8];
+  readFromMemory(fd, address + 0x0C, 4, bytes);
+
+  printf(".------------------------------------------.");
+  printf("\n| %ld (Gk, DL/R, ST)\n", hexBytesToInt(bytes, 4));
+
+  readFromMemory(fd, address - 0x78, 4, bytes);
+  printf("| Ability: %d/%d\n", bytes[0], bytes[2]);
+  printf(".------------------------------------------.");
+  printf("------------------------------------------.\n");
+
+  readFromMemory(fd, address + 0x78, 8, bytes);
+  unsigned char hidden[9];
+  readFromMemory(fd, address - 0x38, 9, hidden);
+  unsigned char byte;
+  readFromMemory(fd, address - 0x2E, 1, &byte);
+
+  printf("| Adaptability:  ");
+  padLeft(bytes[0]);
+  printf("  Professionalism:  ");
+  padLeft(bytes[4]);
+  printf("  | Consistency:    ");
+  padLeft(hidden[3]);
+  printf("  Injury proneness:  ");
+  padLeft(hidden[7]);
+  printf("\n| Ambition:      ");
+  padLeft(bytes[1]);
+  printf("  Sportsmanship:    ");
+  padLeft(bytes[5]);
+  printf("  | Determination:  ");
+  padLeft(byte);
+  printf("  Versatility:       ");
+  padLeft(hidden[8]);
+  printf("\n| Loyalty:       ");
+  padLeft(bytes[2]);
+  printf("  Temperament:      ");
+  padLeft(bytes[6]);
+  printf("  | Dirtiness:      ");
+  padLeft(hidden[0]);
+  printf("\n| Pressure:      ");
+  padLeft(bytes[3]);
+  printf("  Controversy:      ");
+  padLeft(bytes[7]);
+  printf("  | Imp. Matches:   ");
+  padLeft(hidden[6]);
+  // Age
+  // Positions
+
+  printf("\n");
 }
